2e8ca1a50b869c72314ecf6f605fdc1821692298
Mario Inga <mario21ic@gmail.com>
2021-07-10 22:53:37 -0500

    Adding first example

---
+++ 0-intro/src/test/java/com/mario21ic/test/utils/CalculadoraTest.java
@@ -0,0 +1,56 @@
+package com.mario21ic.test.utils;
+
+import com.mario21ic.utils.Calculadora;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class CalculadoraTest {
+
+    @Test
+    @DisplayName("suma de dos numeros")
+    void testSumar() {
+//        fail("Not yet implemented");
+        // arrange
+        Calculadora calc = new Calculadora();
+        // act
+        double suma = calc.sumar(2, 2);
+        // assert
+        assertEquals(4, suma);
+    }
+
+    @Test
+    @DisplayName("resta de dos numeros")
+    void testRestar() {
+        // arrange
+        Calculadora calc = new Calculadora();
+        // act
+        double resta = calc.restar(2, 2);
+        // assert
+        assertEquals(0, resta);
+    }
+
+    @Test
+    @DisplayName("multiplicacion de dos numeros")
+    void testMultiplicar() {
+        // arrange
+        Calculadora calc = new Calculadora();
+        // act
+        double multiplica = calc.multiplicar(2, 3);
+        // assert
+        assertEquals(6, multiplica);
+    }
+
+    @Test
+    @DisplayName("division de dos numeros")
+    void testDividir() {
+        // arrange
+        Calculadora calc = new Calculadora();
+        // act
+        double division = calc.dividir(4, 2);
+        // assert
+        assertEquals(2, division);
+    }
+}
